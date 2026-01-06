fc6f6b58dc86a627a4bad4e8839f24ec7926f6db
Twingamerdudes <81382687+Twingamerdudes@users.noreply.github.com>
2026-01-05 19:11:24 -0500

    Removing all of these AI slop projects from this route
    god, why the fuck did i make any of these projects
    
    Projects Removed:
    Neural symphony
    ai_rick
    ai_engine
    ai_director

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,14 +2,9 @@
     let projects = [
         {name: "All of my itch.io games", description: "There's so much I can't list them here", link: "https://twingamerdudes.itch.io/"},
         {name: "Stickman Sandbox", description: "Apart of a defunct game studio, not on my itch.io page", link: "https://ohm-interactive.itch.io/stickman-sandbox"},
-        {name: "ai_director", description: "The first ever ai_show engine to support stage directions", link: "https://github.com/Twingamerdudes/ai_director"},
-        {name: "ai_engine", description: "Open source ai_sponge", link: "https://github.com/Twingamerdudes/ai_engine"},
-        {name: "ai_rick", description: "AI Rick and Morty. What's better than infinite content!", link: "https://www.youtube.com/watch?v=q9Sxrkk7Sm8"},
         {name: "Lua for NEM", description: "A mod to NEM (which is a mod of M:PN), which adds Lua to it", link: "https://github.com/Twingamerdudes/Lua-For-NEM"},
         {name: "Chaos Mod", description: "A mod for Five Nights at Freddy's Security Breach which makes everything chaotic", link: "https://github.com/Twingamerdudes/Chaos-Mod"},
         {name: "Tire", description: "A programming language made in python", link: "https://github.com/Twingamerdudes/Tire"},
-        {name: "EngScript", description: "English, as a programming language", link: "https://github.com/Twingamerdudes/EngScript"},
-        {name: "Neural Symphony", description: "Fully AI Generated music, one of the few left on spotify", link: "https://www.neuralsymphony.tech/"},
         {name: "Tribe", description: "A tool for simulating lifeforms", link: "https://github.com/Twingamerdudes/Tribe"},
         {name: "Copilot Language", description: "A programming language made using only AI", link: "https://github.com/Twingamerdudes/Copilot-Language"},
         {name: "XGen", description: "A FNF No-Code Engine", link: "https://github.com/Twingamerdudes/XGen"}
